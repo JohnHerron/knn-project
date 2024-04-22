5c3ccd2d87a9ff496b09e71ac0218113b1c85854
John Herron <herroncjohn@gmail.com>
2024-04-22 16:30:02 -0700

    ADD: seed data formatted to csv, data imported and formatted in script

--- main.py
+++ main.py
@@ -1,22 +1,49 @@
 from sklearn.neighbors import KNeighborsClassifier
-from dataclasses import dataclass
+from sklearn.utils import Bunch
+import numpy as np
+import csv
+from os import getcwd
 
 def main():
     seed_data = load_seeds()
+    print (seed_data.data)
     # TODO: knn classification
 
 def load_seeds():
-    data = []
-    target = []
+    data_file_name = "seeds.csv"
+    data, target, target_names = load_csv_data(data_file_name=data_file_name)
+    feature_names = [
+        "area A",
+        "perimeter P", 
+        "compactness C = 4*pi$A/P^2", 
+        "length of kernel",
+        "width of kernel",
+        "asymmetry coefficient",
+        "length of kernel groove"
+    ]
 
-    # TODO: parse text file
+    return Bunch(
+        data=data,
+        target = target,
+        feature_names= feature_names,
+        filename=data_file_name
+    )
 
-    return seed_data(data, target)
+def load_csv_data(data_file_name):
+    with open(data_file_name, "r", encoding="utf-8") as csv_file:
+        data_file = csv.reader(csv_file)
+        temp = next(data_file)
+        n_samples = int(temp[0])
+        n_features = int(temp[1])
+        target_names = np.array(temp[2:])
+        data = np.empty((n_samples, n_features))
+        target = np.empty((n_samples,), dtype=int)
 
-@dataclass
-class seed_data:
-    data: list
-    target = list
+        for i, ir in enumerate(data_file):
+            data[i] = np.asarray(ir[:-1], dtype=np.float64)
+            target[i] = np.asarray(ir[-1], dtype=int)
+
+        return data, target, target_names
 
 if __name__ == "__main__":
     main()
