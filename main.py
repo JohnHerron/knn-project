f451b1d517f6d48e872a02b6a41b7c910ae6ae41
John Herron <herroncjohn@gmail.com>
2024-04-22 15:48:53 -0700

    ADD: seed_data dataclass

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
+from sklearn.neighbors import KNeighborsClassifier
+from dataclasses import dataclass
 
+def main():
+    seed_data = load_seeds()
+    # TODO: knn classification
+
+def load_seeds():
+    data = []
+    target = []
+
+    # TODO: parse text file
+
+    return seed_data(data, target)
+
+@dataclass
+class seed_data:
+    data: list
+    target = list
 
 if __name__ == "__main__":
-    pass
+    main()
