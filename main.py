67c86cb38ba487fd2c81915fa906819fb76accc9
John Herron <herroncjohn@gmail.com>
2024-04-26 19:54:50 -0700

    ADD: KNN classifier error prediction

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import cross_val_predict
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import KFold
+import matplotlib.pyplot as plt
 from sklearn.utils import Bunch
 import numpy as np
 import csv
@@ -6,8 +12,25 @@ import csv
 
 def main():
     seed_data = load_seeds()
-    print (seed_data.data)
-    # TODO: knn classification
+
+    # knn classification
+    x = seed_data.data
+    y = seed_data.target
+    classifier_pipeline = make_pipeline(StandardScaler(), KNeighborsClassifier(n_neighbors=15))
+    cv = KFold(n_splits=5, random_state=0, shuffle=True)
+    y_pred = cross_val_predict(classifier_pipeline, x, y, cv=cv)
+    print(mean_squared_error(y, y_pred))
+    print(r2_score(y, y_pred))
+
+    # test for k, which produces lowest error?
+    error = []
+    for k in range(1,51):
+        classifier_pipeline = make_pipeline(StandardScaler(), KNeighborsClassifier(n_neighbors=k))
+        y_pred = cross_val_predict(classifier_pipeline, x, y, cv=5)
+        error.append(mean_squared_error(y,y_pred))
+    plt.plot(range(1,51),error)
+    plt.show()
+
 
 def load_seeds():
     data_file_name = "seeds.csv"
